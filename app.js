f559f45f79a9202ee721fc6fc2c5151a5c2e2291
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2022-06-06 12:47:04 +0700

    google

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const colors = require("colors");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const methodOverride = require('method-override')
 const path = require('path');
 const passport = require('passport')
 const session = require('express-session')
@@ -16,20 +17,48 @@ dotenv.config({ path: "./config/config.env" });
 // Passport config
 require('./config/passport')(passport)
 
-
-
 connectDB();
 
 const app = express();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
 
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-//Handlebars
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs')
 
-app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+//Handlebars
+app.engine('.hbs', exphbs.engine({ helpers: {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+}, extname: '.hbs', defaultLayout: "main"}));
 app.set("view engine", ".hbs");
 
 // Sessions
@@ -39,8 +68,6 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({mongoUrl: process.env.MONGO_URI,}),
-    
-    
   })
 )
 
@@ -48,13 +75,19 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
-
 
 // Routes
 app.use("/", require("./routes/index"));
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000;
 
